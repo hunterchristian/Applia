ba6b7961e6dda176a691caa4596078c81c77df1d
Hunter Hodnett <hchodnett@gmail.com>
2019-06-30 21:52:36 -0500

    Add Source Code Pro font (#6)

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,16 @@ const commonConfig = {
         loader: 'ts-loader',
       },
       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+      {
         test: /\.(ts|tsx)$/,
         enforce: 'pre',
         use: [{

ceda98c5928e1b4e5fb91c26a6a87e549004587e
Hunter Hodnett <hchodnett@gmail.com>
2019-07-04 21:44:02 -0500

    Add screenshots to tests

--- functional-tests/dragndrop.test.js
+++ functional-tests/dragndrop.test.js
@@ -14,13 +14,7 @@ describe('Application launch', () => {
   });
 
   it('should drag and element from the palette and drop it on the canvas', async () => {
-    const dab = await this.app.client.element('.palette-element');
-    const canvas = await this.app.client.element('.node');
-
-    await this.app.client.moveToObject('.palette-element');
-    await this.app.client.buttonDown(0);
-    await this.app.client.moveToObject('.node');
-    await this.app.client.buttonUp(0);
+    await this.app.client.dragAndDrop('.palette-element', '.node');
     await sleep(2000);
     const elem = await this.app.client.element('.node > .node');
     expect(elementExists(elem)).toBe(true);
